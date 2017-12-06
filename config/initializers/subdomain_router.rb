1f5b38d2cd02d0191859f540910fbcda1e21f7ea
Kirill <c.derche@me.com>
2017-12-06 17:59:01 +0300

    added domain env for prod

--- config/initializers/subdomain_router.rb
+++ config/initializers/subdomain_router.rb
@@ -1,5 +1,5 @@
 # SubdomainRouter::Config.default_subdomain = 'www'
-# SubdomainRouter::Config.domain = 'mywebsite.com'
+SubdomainRouter::Config.domain = ENV['DOMAIN']
 # SubdomainRouter::Config.tld_components = 1
 SubdomainRouter::Config.subdomain_matcher = ->(subdomain, request) {
   Account.find_by(subdomain: subdomain)
